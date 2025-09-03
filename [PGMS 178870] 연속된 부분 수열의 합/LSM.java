7d8b1a67de4ceabdabb3aaa66b608eb34e6ebbd1
Sang-Myeong Lee <teamjuji5@naver.com>
2025-09-03 16:01:04 +0900

    Update LSM.java

--- [PGMS 178870] 연속된 부분 수열의 합/LSM.java
+++ [PGMS 178870] 연속된 부분 수열의 합/LSM.java
@@ -1,1 +1,39 @@
 
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int[] solution(int[] sequence, int k) {
+        int size = sequence.length;
+        
+        int l = 0, r = 0;
+        int sum = sequence[l];
+        
+        int minLen = size + 1;
+        int[] minLR = new int[2];
+        
+        while(l < size && r < size) {
+            if (sum == k) {
+                if (r - l + 1 < minLen) {
+                    minLR[0] = l;
+                    minLR[1] = r;
+                    minLen = r - l + 1;
+                }
+                
+                sum -= sequence[l];
+                l++;
+            }
+            else if (sum < k) {
+                r++;
+                if (r < size)
+                    sum += sequence[r];
+            }
+            else {
+                sum -= sequence[l];
+                l++;
+            }
+        }
+        
+        return minLR;
+    }
+}
