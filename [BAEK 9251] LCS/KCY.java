947c7826585646cd940b9be139145f33efe0d8e6
YoungE <56716209+kchy0625@users.noreply.github.com>
2025-09-04 15:48:20 +0900

    Update KCY.java

---
+++ [BAEK 9251] LCS/KCY.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+
+        int[][] dp = new int[arr1.length + 1][arr2.length + 1];
+
+        for (int i = 1; i <= arr1.length; i++) {
+            for (int j = 1; j <= arr2.length; j++) {
+                if (arr1[i - 1] == arr2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        System.out.println(dp[arr1.length][arr2.length]); // LCS 길이 출력
+    }
+}
