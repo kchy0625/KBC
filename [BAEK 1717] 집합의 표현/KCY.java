8a1f68d650158f9cd3337c0bb005ffed12991cc0
YoungE <56716209+kchy0625@users.noreply.github.com>
2025-09-05 13:50:08 +0900

    Update KCY.java

---
+++ [BAEK 1717] 집합의 표현/KCY.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int parent[];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        parent = new int[n + 1];
+
+        for (int i = 0; i < n + 1; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int question = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (question == 0) {
+                union(a, b);
+            } else {
+                boolean result = checkSame(a,b);
+                if (result) {
+                    System.out.println("YES");
+                }else{
+                    System.out.println("NO");
+                }
+            }
+        }
+    }
+
+    private static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a != b) {
+            parent[b] = a;
+        }
+    }
+
+    private static int find(int a) {
+        if (parent[a] == a) {
+            return a;
+        } else {
+            return parent[a] = find(parent[a]);
+        }
+    }
+
+    private static boolean checkSame(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a == b) {
+            return true;
+        }
+        return false;
+    }
+}
