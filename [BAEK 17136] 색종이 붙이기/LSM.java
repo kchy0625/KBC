ee8322c4ce1937195e104bac1c3efa92cef8bec5
Sang-Myeong Lee <teamjuji5@naver.com>
2025-10-16 16:33:05 +0900

    Update LSM.java

--- [BAEK 17136] 색종이 붙이기/LSM.java
+++ [BAEK 17136] 색종이 붙이기/LSM.java
@@ -1,19 +1,16 @@
-
 import java.util.*;
 import java.io.*;
 
 public class Main {
     
-    public static int[][] map;
-    public static int min = 30;
+    public static int[][] map = new int[10][10];
+    public static int min = Integer.MAX_VALUE;
     public static int[] papers = { 0, 5, 5, 5, 5, 5 };
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
-        
-        map = new int[10][10];
         
         for(int i = 0; i < 10; i++) {
             st = new StringTokenizer(br.readLine());
@@ -22,11 +19,10 @@ public class Main {
             }
         }
         
-        dfs(0,0,0);
+        dfs(0, 0);
         
-        if (min == 30) {
+        if (min == Integer.MAX_VALUE)
             min = -1;
-        }
         
         bw.write(min + "");
         bw.flush();
@@ -36,17 +32,15 @@ public class Main {
         
     }
     
-    public static void dfs(int x, int y, int cnt) {
+    public static void dfs(int xy, int cnt) {
         
-        if (x >= 9 && y >= 10) {
-            min = Math.min(min, cnt);
+        if (xy >= 100) {
+            min = Math.min(cnt, min);
             return;
         }
         
-        if (y >= 10) {
-            dfs(x+1, 0, cnt);
-            return;
-        }
+        int x = xy / 10;
+        int y = xy % 10;
         
         if (map[x][y] == 1) {
             for(int i = 5; i >= 1; i--) {
@@ -54,7 +48,7 @@ public class Main {
                     attach(x, y, i, 0);
                     papers[i]--;
                     
-                    dfs(x, y+1, cnt + 1);
+                    dfs(xy + 1, cnt + 1);
                     
                     papers[i]++;
                     attach(x, y, i, 1);
@@ -62,12 +56,24 @@ public class Main {
             }
         }
         else {
-            if (y+1 < 10)
-                dfs(x, y+1, cnt + 1);
-            else
-                dfs(x+1, y, cnt + 1);
+            dfs(xy + 1, cnt);
         }
         
+    }
+    
+    public static boolean isAttachable(int x, int y, int size) {
+        for(int i = x; i < x + size; i++) {
+            for(int j = y; j < y + size; j++) {
+                
+                if (i < 0 || i >= 10 || j < 0 || j >= 10)
+                    return false;
+                
+                if (map[i][j] == 0)
+                    return false;
+            }
+        }
+        
+        return true;
     }
     
     public static void attach(int x, int y, int size, int state) {
@@ -76,19 +82,5 @@ public class Main {
                 map[i][j] = state;
             }
         }
-    }
-    
-    public static boolean isAttachable(int x, int y, int size) {
-        for(int i = x; i < x + size; i++) {
-            for(int j = y; j < y + size; j++) {
-                if(i < 0 || i >= 10 || j < 0 || j >= 10)
-                    return false;
-            
-                if (map[i][j] != 1)
-                    return false;
-            }
-        }
-        
-        return true;
     }
 }
